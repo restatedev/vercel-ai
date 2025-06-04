fce033238cf92ab5cce9297ecffa2190e390dc34
igalshilman <igal@restate.dev>
2025-06-04 19:25:32 +0200

    Add offset query param

--- app/pubsub/[topic]/route.ts
+++ app/pubsub/[topic]/route.ts
@@ -1,10 +1,15 @@
 import { sseStream } from "@/restate/pubsub_client";
+import { NextRequest } from "next/server";
 
-export async function GET(request: Request, { params }: any) {
+export async function GET(request: NextRequest, { params }: any) {
   const topic = (await params).topic;
+  const searchParams = request.nextUrl.searchParams;
+  const offsetQuery = Number(searchParams.get("offset") || 0);
+  const offset = isNaN(offsetQuery) ? 0 : offsetQuery;
   const stream = sseStream({
     ingressUrl: process.env.INGRESS_URL || "http://localhost:8080",
     topic,
+    offset
   });
   return new Response(stream, {
     headers: {
