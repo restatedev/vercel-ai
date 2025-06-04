f6a11c5de242d9670840f7ffa4cf8f687b15611c
Nik Nasr <nik@restate.dev>
2025-06-04 16:46:31 +0100

    add retry logic
    Signed-off-by: Nik Nasr <nik@restate.dev>

--- app/chat/[topic]/page.tsx
+++ app/chat/[topic]/page.tsx
@@ -38,6 +38,12 @@ export default function Chat() {
         };
         evtSource.onerror = (error) => {
           console.error("EventSource failed:", error);
+
+          if (evtSource.readyState === EventSource.CLOSED) {
+            setTimeout(() => {
+              obtainAPIResponse();
+            }, 1000);
+          }
         };
       } catch (error) {}
     };
